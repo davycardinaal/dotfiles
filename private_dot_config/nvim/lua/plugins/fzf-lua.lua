be80317ace2fed5ddc8fce2a5f5489fcacf35e1e
Davy Cardinaal <davy@jobport.nl>
2025-01-07 15:15:10 +0100

    Grep in files from cwd by default

--- private_dot_config/nvim/lua/plugins/fzf-lua.lua
+++ private_dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -2,5 +2,6 @@ return {
   "ibhagwan/fzf-lua",
   keys = {
     { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+    { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
   },
 }
