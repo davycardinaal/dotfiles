80d87a7be0ae88ed74a03de9a02ab0fd3aaf2690
Davy Cardinaal <davy@jobport.nl>
2025-01-07 15:44:55 +0100

    Support extra languages via treesitter

---
+++ private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "css",
+        "javascript",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "pug",
+        "scss",
+        "typescript",
+        "vue",
+        "yaml",
+      },
+    },
+  },
+}
